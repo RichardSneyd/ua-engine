6e00f5ac0dad4d7ee5f711453ef9fc3362730e95
Rudrabhoj <contact.pinbartrader@gmail.com>
2020-04-02 19:29:37 +0530

    next test

--- src/Game/UAENGINE.ts
+++ src/Game/UAENGINE.ts
@@ -5,21 +5,12 @@ import Loader from './Core/Engine/Loader';
 import Events from './Core/Engine/Events';
 import LevelManager from './Core/Engine/LevelManager';
 
-namespace UAENGINE {
-  let world: World;
-  let entity: Entity;
-  let loop: Loop;
-  let loader: Loader;
-  let events: Events;
-  let levelManager: LevelManager;
-
-  function foo() {
-      world;
-      entity;
-      loop;
-      loader;
-      events;
-      levelManager;
-  }
+class UAENGINE {
+  public static world: World;
+  public static entity: Entity;
+  public static loop: Loop;
+  public static loader: Loader;
+  public static events: Events;
+  public static levelManager: LevelManager;
 }
 export default UAENGINE;
