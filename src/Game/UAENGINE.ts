a43f129b8d4f7343d7b9a8d40d7fc6280905ea57
Rudrabhoj <contact.pinbartrader@gmail.com>
2020-04-02 19:21:54 +0530

    defines for  UAENGINE

---
+++ src/Game/UAENGINE.ts
@@ -0,0 +1,16 @@
+import World from './Core/Engine/World';
+import Entity from './Core/Engine/Entity';
+import Loop from './Core/Engine/Loop';
+import Loader from './Core/Engine/Loader';
+import Events from './Core/Engine/Events';
+import LevelManager from './Core/Engine/LevelManager';
+
+namespace UAENGINE {
+  let world: World;
+  let entity: Entity;
+  let loop: Loop;
+  let loader: Loader;
+  let events: Events;
+  let levelManager: LevelManager;
+}
+export default UAENGINE;
