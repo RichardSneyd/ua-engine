b0556da0e46f0514f04f326aae42bcf9e2486856
Rudrabhoj <contact.pinbartrader@gmail.com>
2020-04-02 19:40:31 +0530

    types compilation streamlined

--- typecompile.sh
+++ typecompile.sh
@@ -1,3 +1,4 @@
-tsc --declaration --target ES2016
-dts-bundle --name UAENGINE --main src/Game/UAENGINE.d.ts
-cp src/Game/UAENGINE.d.ts build/UAENGINE.d.ts
+tsc --declaration --target ES2016 --outDir js/types
+dts-bundle --name UAENGINE --main js/types/Game/UAENGINE.d.ts
+cp js/types/Game/UAENGINE.d.ts build/UAENGINE.d.ts
+rm -rf js/types
