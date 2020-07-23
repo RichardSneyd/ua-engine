c0373221e4e84ae70aa0ccc40c867f05dca997c5
Richard_Sneyd <richrdsneyd@hotmail.com>
2020-07-23 15:53:09 +0100

    structured tasks in Gulpfile, ready to begin testing

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,87 +1,67 @@
 var gulp = require("gulp"),
-	webserver = require("gulp-webserver"),
-	notify = require("gulp-notify"),
-	sourcemaps = require("gulp-sourcemaps"),
-	browserify = require("browserify"),
-	watchify = require("watchify"),
-	source = require("vinyl-source-stream"),
-	buffer = require("vinyl-buffer"),
-	gutil = require("gulp-util");
+    webserver = require("gulp-webserver"),
+    notify = require("gulp-notify"),
+    sourcemaps = require("gulp-sourcemaps"),
+    browserify = require("browserify"),
+    tsify = require("tsify"),
+    watchify = require("watchify"),
+    source = require("vinyl-source-stream"),
+    buffer = require("vinyl-buffer"),
+    gutil = require("gulp-util"),
+    conc = require("gulp-concat"),
+    uglify = require("gulp-uglify"),
+    pipeline = require('readable-stream').pipeline;
 
-gulp.task("js", function() {
-	return gulp.src("src/js/**/*.js")
-		.pipe(gulp.dest("bin/js/"));
-});
 
-gulp.task("js", function() { return buildJS(); });
+function build(){
+    var b = browserify({
+        debug: true,
+        cache: {},
+        packageCache: {},
+        fullPaths: watch
+    }).plugin(tsify).transform("babelify", {
+        presets: ['@babel/preset-env'],
+            extensions: ['.ts']
+    });
 
-function handleError(error) {
-	notify.onError({
-		title: "Build Error!",
-		message: "<%= error.message %>"
-	})(error);
+    b.bundle()
+    .on("error", handleError)
+    .pipe(source("src/main.ts"))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("js/game/"));
 
-	this.emit("end");
 }
 
-function buildJS(watch) {
-	var browserifyInstance = browserify({
-			entries: ["src/js/main.js"],
-			debug: true,
-			cache: {},
-		    packageCache: {},
-		    fullPaths: watch
-		}).transform("stringify", {
-	        appliesTo: { includeExtensions: [".vert", ".frag", ".glsl"] }
-	    }).transform("babelify", {
-			presets: ["es2015"]
-		});
-
-	var b = watch ? watchify(browserifyInstance) : browserifyInstance;
-
- 	var build = function() {
-		return b.bundle()
-			.on("error", handleError)
-			.pipe(source("main.js"))
-			.pipe(buffer())
-			.pipe(sourcemaps.init({ loadMaps: true }))
-			.pipe(sourcemaps.write("."))
-			.pipe(gulp.dest("bin/js/"));
-	}
-
-	if (watch) {
-		b.on("update", function() {
-			gutil.log("Rebundling...");
-			build();
-		});
-		b.on("log", function(e) {
-			gutil.log("Bundling Successful: " + gutil.colors.gray(e));
-		});
-	}
-
-	return build();
-}
-
-gulp.task("html", function() {
-	return gulp.src("src/html/**/*.html")
-		.pipe(gulp.dest("bin/"));
+gulp.task("webserver", function () {
+    gulp.src("./")
+    .pipe(webserver({
+        livereload: true,
+        open: true
+    }));  
 });
 
-gulp.task("images", function() {
-	return gulp.src("images/**/*")
-		.pipe(gulp.dest("bin/images/"));
+gulp.task("browserify", function(){
+    this.build();
+})
+
+gulp.task("watch", function(){
+    gulp.watch('src/**/*.ts', ['build', 'watch'])
 });
 
-gulp.task("watch", ["html"], function() {
-	gulp.watch("src/html/**/*.html", ["html"]);
-	gulp.watch("images/**/*", ["images"]);
-	buildJS(true);
+gulp.task('uglify', function () {
+    return pipeline(
+          gulp.src('lib/*.js'),
+          uglify(),
+          gulp.dest('lib')
+    );
+  });
 
-	gulp.src("bin")
-		.pipe(webserver({
-			livereload: true,
-			open: true
-		}));
-});
+gulp.task('concat', function(){
+    return gulp.src(['./lib/rise_h5_sdk_v1.0.js', './dist/uae.js'])
+    .pipe(concat('uae.js'))
+    .pipe(gulp.dest('./dist/'));
+})
 
-gulp.task("default", ["js", "html", "images"]);
+gulp.task("default", ["build", "watch", "concat", "webserver"]);
