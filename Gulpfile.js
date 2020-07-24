9dd54ffe9c0c8e54116af72c160c2f68535512a7
Richard_Sneyd <richrdsneyd@hotmail.com>
2020-07-24 10:59:45 +0100

    building, with watchify. good rebuild times

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,65 +3,97 @@ var gulp = require("gulp"),
     notify = require("gulp-notify"),
     sourcemaps = require("gulp-sourcemaps"),
     browserify = require("browserify"),
-    tsify = require("tsify"),
     watchify = require("watchify"),
+    gutil = require("gulp-util"),
+    tsify = require("tsify"),
     source = require("vinyl-source-stream"),
     buffer = require("vinyl-buffer"),
-    gutil = require("gulp-util"),
-    conc = require("gulp-concat"),
+    concat = require("gulp-concat"),
     uglify = require("gulp-uglify"),
     pipeline = require('readable-stream').pipeline;
 
+function handleError(error) {
+    notify.onError({
+        title: "Build Error!",
+        message: "<%= error.message %>"
+    })(error);
 
-function build(){
-    var b = browserify({
+    this.emit("end");
+}
+
+gulp.task('build', function () {
+    build();
+    return;
+});
+
+function build(watch) {
+    var browserifyInstance = browserify({
+        entries: ['src/main.ts'],
         debug: true,
         cache: {},
         packageCache: {},
         fullPaths: watch
     }).plugin(tsify).transform("babelify", {
         presets: ['@babel/preset-env'],
-            extensions: ['.ts']
-    });
+        extensions: ['.ts']
+    };
 
-    b.bundle()
-    .on("error", handleError)
-    .pipe(source("src/main.ts"))
-    .pipe(buffer())
-    .pipe(sourcemaps.init({ loadMaps: true }))
-    .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest("js/game/"));
+    var b = watch ? watchify(browserifyInstance) : browserifyInstance;
 
+    var build = function () {
+        return b.bundle()
+            .on("error", handleError)
+            .pipe(source("uae.js"))
+            .pipe(buffer())
+            .pipe(sourcemaps.init({ loadMaps: true }))
+            .pipe(sourcemaps.write("."))
+            .pipe(gulp.dest("dist/"));
+    }
+
+    if (watch) {
+		b.on("update", function() {
+			gutil.log("Rebundling...");
+			build();
+		});
+		b.on("log", function(e) {
+			gutil.log("Bundling Successful: " + gutil.colors.gray(e));
+		});
+	}
+
+	return build();
 }
 
 gulp.task("webserver", function () {
     gulp.src("./")
-    .pipe(webserver({
-        livereload: true,
-        open: true
-    }));  
+        .pipe(webserver({
+            livereload: true,
+            open: true
+        }));
 });
 
-gulp.task("browserify", function(){
-    this.build();
+gulp.task("browserify", function () {
+    return build();
 })
 
-gulp.task("watch", function(){
-    gulp.watch('src/**/*.ts', ['build', 'watch'])
+gulp.task("watch", function () {
+  //  return gulp.watch('src/**/*.ts', gulp.series(['build', 'watch']));
+  return build(true);
 });
 
 gulp.task('uglify', function () {
     return pipeline(
-          gulp.src('lib/*.js'),
-          uglify(),
-          gulp.dest('lib')
+        gulp.src('lib/*.js'),
+        uglify(),
+        gulp.dest('lib')
     );
-  });
+});
 
-gulp.task('concat', function(){
+gulp.task('concat', function () {
     return gulp.src(['./lib/rise_h5_sdk_v1.0.js', './dist/uae.js'])
-    .pipe(concat('uae.js'))
-    .pipe(gulp.dest('./dist/'));
+        .pipe(concat('uae.js'))
+        .pipe(gulp.dest('./dist/'));
 })
 
-gulp.task("default", ["build", "watch", "concat", "webserver"]);
+gulp.task("default", function () {
+    return gulp.series('watch', 'concat');
+});
--- typecompile_OLD.sh
+++
@@ -1,4 +0,0 @@
-tsc --declaration --target es2015 --outDir js/types
-dts-bundle --name UAENGINE --main js/types/Game/UAENGINE.d.ts
-cp js/types/Game/UAENGINE.d.ts build/UAENGINE.d.ts
-rm -rf js/types
