11e3e377286cb15317f684543f3ae6f9d6724c31
Can Delibas <candelibas96@gmail.com>
2020-12-23 21:44:35 +0200

    add inspector instructions

--- src/Game/Core/Engine/LevelEditor/Inspector.ts
+++ src/Game/Core/Engine/LevelEditor/Inspector.ts
@@ -1,5 +1,27 @@
 class Inspector {
+    constructor() {
+        this._init();
+    }
 
+    _init(): void {
+        //TODO: initialize event manager through game objects
+    }
+
+    public createInspector(): void {
+
+    }
+
+    addHeader(title: string, container: HTMLDivElement) {
+
+    }
+
+    addNumberInput(): void {
+
+    }
+
+    addTextInput(): void {
+
+    }
 }
 
 export default Inspector;
