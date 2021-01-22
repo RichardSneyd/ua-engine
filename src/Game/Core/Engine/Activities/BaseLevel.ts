b5c9815915231273680e8bfcfc6e4af6acfb00ff
RichardSneyd <richardsneyd@hotmail.com>
2021-01-22 13:50:06 +0000

    added conditional character preload to BaseLevel

--- src/Game/Core/Engine/Activities/BaseLevel.ts
+++ src/Game/Core/Engine/Activities/BaseLevel.ts
@@ -82,6 +82,9 @@ abstract class BaseLevel extends BaseScene implements ILevel {
      * @description adds resources to the load queue, then uses a promise to download those resource, then call the start method
      */
     preload(): void {
+        if(!this._manager.script.isFalsy(this.configRow.config.char)){
+            this._loader.addSpine(this.configRow.config.char);
+        }
         super.preload();
     }
 
